4ddc66bc9b0fea890db5a06b81fae2658f475d41
Peter <dkdndes@gmail.com>
2017-02-13 18:03:39 +0100

    updated context processor and login url

--- subscribe/tests/urls.py
+++ subscribe/tests/urls.py
@@ -11,6 +11,9 @@ from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls.static import static
 
+from django.contrib.auth import views as auth_views
+
+url(r'^accounts/login/$', auth_views.login),
 
 admin.autodiscover()
 
@@ -18,7 +21,6 @@ urlpatterns = static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += staticfiles_urlpatterns()
 urlpatterns += [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^accounts/login/$', 'django.contrib.auth.views.login',
-        name='auth_login'),
+    url(r'^accounts/login/$', auth_views.login),
     url(r'^', include('subscribe.urls')),
 ]
